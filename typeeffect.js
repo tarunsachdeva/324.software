0a2414924fa261e8afad544c8fd1738d6ed34f98
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2021-10-15 07:22:34 -0400

    updates

---
+++ typeeffect.js
@@ -0,0 +1,11 @@
+  var typed = new Typed(".typed", {
+  strings: ["product studio", "microfund", "creative workshop",  "space for learning", "container for multidisciplinary experiments"],
+  typeSpeed: 30,
+  backSpeed: 20,
+  backDelay: 800,
+  startDelay: 500,
+  loop: false,
+  showCursor: true,
+  cursorChar: "_",
+  attr: null,
+});
